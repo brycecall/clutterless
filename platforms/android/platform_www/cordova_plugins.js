4bd41815115bba92890e1e5dae89d2dd62a89502
Bryce Call <brycecall@gmail.com>
2016-03-30 20:43:34 -0600

    refreshed platform install

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,28 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
-    },
-    {
-        "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
-        "id": "cordova-connectivity-monitor.connectivity",
-        "clobbers": [
-            "window.connectivity"
-        ]
-    },
-    {
-        "file": "plugins/cordova-admob/www/admob.js",
-        "id": "cordova-admob.AdMobAds",
-        "clobbers": [
-            "window.admob",
-            "window.tappx"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-google-play-services": "25.0.0",
+    "cordova-plugin-extension": "1.3.0",
+    "cordova-plugin-admobpro": "2.14.0",
+    "cordova-plugin-whitelist": "1.2.1"
+};
 // BOTTOM OF METADATA
 });
